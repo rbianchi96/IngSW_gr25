875bca0994dbd724adf932d47d75ea31c0e18cc6
rbianchi96 <roberto17.bianchi@mail.polimi.it>
2018-04-26 19:32:22 +0200

    Aggiornate dipendenze per Junit.

--- src/test/java/it/polimi/ingsw/AppTest.java
+++ src/test/java/it/polimi/ingsw/AppTest.java
@@ -1,38 +1,14 @@
 package it.polimi.ingsw;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+public class AppTest {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void myFirstTest() {
+		assertEquals(2, 1 + 1);
+		assertEquals(2, 1 + 2);
+	}
 }
