2c7027c4b131ef3972cf68d6038053f6e20c3f6d
DiegoCaro96 <37809116+DiegoCaro96@users.noreply.github.com>
2018-05-05 19:36:46 +0200

    Changes at DiceBag and DiceBagtest

--- src/main/java/it/polimi/ingsw/DiceBag.java
+++ src/main/java/it/polimi/ingsw/DiceBag.java
@@ -6,41 +6,40 @@ public class DiceBag {
     private ArrayList<Dice> dices;
     private static int min_size = 1;
 
-
+    // DiceBag constructor: it creates the Dice bag, and the value of each dice is initialized at -1,
+    // because when they are inside the Dice bag we can't establish they're value, when a dice will be extracted,
+    // its value will be set
     public DiceBag() {
         dices = new ArrayList<Dice>();
         for (Color color : Color.values()) {
             for (int j = 0; j < 18; j++) {
-                    dices.add(new Dice(-1, color));
-                }
+                dices.add(new Dice(-1, color));
             }
         }
-
-
-        public int getsize(){
-            return dices.size();
-        }
-
-        public Dice getRandomDice () {
-            if (dices.size() >= min_size) {
-                int i = (int) Math.random() * (dices.size() - 1);
-                Dice d = dices.get(i);
-                dices.remove(i);
-                return d;
-            }
-            return null;
-        }
-
-
-
-        public String toString () {
-            String s = "DiceBag:" + "\n" + "number of Dice " + dices.size() + "\n";
-            for (Dice d : dices) {
-                s = s + d.toString() + "\n";
-            }
-            return s;
-        }
-
     }
+
+    public int getSize() {
+        return dices.size();
+    }
+
+    // method that takes one casual dice from the bag
+    public Dice getRandomDice() {
+        if (dices.size() >= min_size) {
+            int i = (int) (Math.random() * (dices.size() - 1));
+            Dice d = dices.get(i);
+            dices.remove(i);
+            return d;
+        }
+        return null;
+    }
+
+    public String toString() {
+        String s = "DiceBag:" + "\n" + "number of Dice " + dices.size() + "\n";
+        for (Dice d : dices) {
+            s = s + d.toString() + "\n";
+        }
+        return s;
+    }
+}
 
 
--- src/test/java/it/polimi/ingsw/DiceBagTest.java
+++ src/test/java/it/polimi/ingsw/DiceBagTest.java
@@ -8,11 +8,25 @@ class DiceBagTest {
 
     @Test
     void getRandomDice() {
+        //creation of a DiceBag
         DiceBag db = new DiceBag();
-        int temporary_size = db.getsize();
+        //verify if it contains 90 dices
+        assertEquals(90, db.getSize());
+        //verify if it removes dices
+        int temporary_size = db.getSize();
         db.getRandomDice();
-        assertEquals(temporary_size-1, db.getsize());
+        assertEquals(temporary_size-1, db.getSize());
         Dice d = db.getRandomDice();
-        assertEquals(temporary_size-2, db.getsize());
+        assertEquals(temporary_size-2, db.getSize());
+        //verify if all the dices are removed by creating a new bag
+        DiceBag db2 = new DiceBag();
+        for(int i=0; i < 90; i++){
+           db2.getRandomDice();
+        }
+        assertEquals(0,db2.getSize());
+        //verify if the size remain 0
+        db2.getRandomDice();
+        assertEquals(0,db2.getSize());
+
     }
 }
