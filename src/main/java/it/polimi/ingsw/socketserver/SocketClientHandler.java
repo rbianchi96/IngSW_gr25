4cea4e947a6401ff33e400d77ffcc522db0172d2
FraCheck <37813781+FraCheck@users.noreply.github.com>
2018-05-14 15:29:11 +0200

    SocketClientHandler update

--- src/main/java/it/polimi/ingsw/socketserver/SocketClientHandler.java
+++ src/main/java/it/polimi/ingsw/socketserver/SocketClientHandler.java
@@ -1,4 +1,21 @@
 package it.polimi.ingsw.socketserver;
 
-public class SocketClientHandler {
+import it.polimi.ingsw.Controller;
+
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class SocketClientHandler implements Runnable{
+    private Socket socket;
+    Scanner in;
+    Controller controller;
+    PrintWriter out;
+    public SocketClientHandler(Socket socket, Controller controller){
+        this.controller = controller;
+        this.socket = socket;
+    }
+    public void run(){}
+
+
 }
