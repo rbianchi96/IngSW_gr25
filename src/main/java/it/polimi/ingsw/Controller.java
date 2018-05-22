0235c9d97fe7ffad411b1787eb213a0a94b8a07b
FraCheck <37813781+FraCheck@users.noreply.github.com>
2018-05-22 15:50:23 +0200

    Controller updated sych methods

--- src/main/java/it/polimi/ingsw/Controller.java
+++ src/main/java/it/polimi/ingsw/Controller.java
@@ -11,17 +11,17 @@ public class Controller {
     }
 
     // Make login request from client to Model
-    public void login(ClientInterface clientInterface, String username){
+    public synchronized void login(ClientInterface clientInterface, String username){
         lobby.login(clientInterface, username);
     }
 
     // Make logout request from client to Model
-    public void logout(ClientInterface clientInterface){
+    public synchronized void logout(ClientInterface clientInterface){
         lobby.logout(clientInterface);
     }
 
     // Notify a lost connection from Socket / Rmi server to Model in order to handle it
-    public void lostConnection(ClientInterface clientInterface){
+    public synchronized void lostConnection(ClientInterface clientInterface){
         lobby.lostConnection(clientInterface);
     }
 
