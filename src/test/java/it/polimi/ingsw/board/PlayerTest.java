1a63cdae3577f06cffbd726828167c3fe7cac2b5
DiegoCaro96 <37809116+DiegoCaro96@users.noreply.github.com>
2018-07-01 20:16:17 +0200

    adding new tests

---
+++ src/test/java/it/polimi/ingsw/board/PlayerTest.java
@@ -0,0 +1,52 @@
+package it.polimi.ingsw.board;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+    
+
+    @Test
+    void FavourTokens() {
+        Player p = new Player("Gianni");
+        p.setFavourTokens(5);
+        assertEquals(5,p.getFavourTokens());
+
+    }
+
+    @Test
+    void ScoreToken() {
+        Player p = new Player("Gianni");
+        p.setScoreToken(Color.RED);
+        assertEquals(Color.RED.toString(),p.getScoreToken().toString());
+    }
+
+    @Test
+    void PlayerName() {
+        Player p = new Player("Gianni");
+        assertEquals("Gianni",p.getPlayerName());
+    }
+
+    @Test
+    void IsOnline() {
+        Player p = new Player("Gianni");
+        assertEquals(true,p.getIsOnline());
+        p.setIsOnline(false);
+        assertEquals(false, p.getIsOnline());
+    }
+
+    @Test
+    void HasPlacedDice() {
+        Player p = new Player("Gianni");
+        p.setHasPlacedDice(true);
+        assertEquals(true, p.getHasPlacedDice());
+    }
+
+    @Test
+    void HasPlayedToolCard() {
+        Player p = new Player("Gianni");
+        p.setHasPlayedToolCard(false);
+        assertEquals(false,p.getHasPlayedToolCard());
+    }
+}
