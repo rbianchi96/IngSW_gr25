2b7c18fc8c720b24e50edcb09c08b26aecdd015d
rbianchi96 <roberto17.bianchi@mail.polimi.it>
2018-07-07 14:40:31 +0200

    Improvement of resource loader.

--- src/main/java/it/polimi/ingsw/ResourcesPathResolver.java
+++ src/main/java/it/polimi/ingsw/ResourcesPathResolver.java
@@ -11,14 +11,21 @@ public class ResourcesPathResolver {
 	 * @throws FileNotFoundException if not exists a file with the given path
 	 */
 	public static Reader getResourceFile(String path, String fileName) throws FileNotFoundException {
-		if(path == null) {	//Built-in resources
+		if(path != null) {
+			try {
+				InputStreamReader external = new InputStreamReader(new FileInputStream(path + fileName));
+
+				return external;
+			} catch(FileNotFoundException e) {
+				System.out.println(fileName + " in use defined folder not found, using built-in resource file.");
+				return getResourceFile(null, fileName);	//Return built-in resource
+			}
+		} else {	//Built-in resources
 			InputStream iS = ResourcesPathResolver.class.getClassLoader().getResourceAsStream(fileName);
 
 			if(iS == null) throw new FileNotFoundException();
 
 			return new InputStreamReader(iS);
-		} else {
-			return new InputStreamReader(new FileInputStream(path + fileName));
 		}
 	}
 }
